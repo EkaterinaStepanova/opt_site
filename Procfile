53f9f4ed25bf661220a34a1729a0c2c6e1c8673c
bird-in-hat <buldakov.95@mail.ru>
2018-06-06 20:34:40 +0300

    python-3.6.4

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn optsite.wsgi --log-file -
+web: gunicorn optsite.wsgi
--- optsite/settings.py
+++ optsite/settings.py
@@ -89,12 +89,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'measures_db',
-        'USER': 'name',
-        'PASSWORD': '',
+        'USER': 'vladislav',
+        'PASSWORD': 'Qwerty123',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
